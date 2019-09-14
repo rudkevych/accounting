04a8378e55868e5d443bc4c91b5b67e37c7bc474
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-09-14 16:06:25 +0200

    form validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'rudkevycho-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
   }
 
 }
