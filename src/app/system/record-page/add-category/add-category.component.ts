f7e769a719231cffa45fd39820716841ea3a1b18
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-09-24 14:45:27 +0300

    category adding

--- src/app/system/record-page/add-category/add-category.component.ts
+++ src/app/system/record-page/add-category/add-category.component.ts
@@ -1,24 +1,34 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {CategoriesService} from '../../shared/services/categories.service';
+import {Category} from '../../shared/models/category.model';
 
 @Component({
   selector: 'rudkevycho-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
+
 export class AddCategoryComponent implements OnInit {
 
-  constructor(categoriesService: CategoriesService) {
+  constructor(private categoriesService: CategoriesService) {
   }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
-    let {name, capacity} = form.value;
+    let {capacity} = form.value;
     if (capacity < 0) {
       capacity *= -1;
     }
+
+    const newCategory = new Category(name, capacity);
+
+    this.categoriesService.addCategory(newCategory)
+      .subscribe((category: Category) => {
+        console.log(category);
+      });
   }
+
 }
--- src/app/system/shared/services/categories.service.ts
+++ src/app/system/shared/services/categories.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Category} from '../models/category.model';
+import {User} from '../../../shared/models/user.model';
+import {Observable} from 'rxjs';
 
 
 @Injectable()
 export class CategoriesService {
   constructor(public http: HttpClient) {}
 
-  addCategory(category: Category) {
-    this.http.post('categories', category);
+  addCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(`http://localhost:3000/categories`, category);
   }
 }
