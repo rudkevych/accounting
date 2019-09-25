e8587f34e83a15ec486e6fbc7b9925caa5c97048
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-09-26 00:01:32 +0300

    add-event validation

--- src/app/system/record-page/add-event/add-event.component.ts
+++ src/app/system/record-page/add-event/add-event.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Category} from '../../shared/models/category.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'rudkevycho-add-event',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
+  @Input() categories: Category[] = [];
+  types = [
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'}
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
   }
 
 }
