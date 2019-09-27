a48fa6c015995429c30c46d0761690ba49b335b0
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-09-27 16:07:17 +0300

    unsubscribing

--- src/app/system/record-page/add-event/add-event.component.ts
+++ src/app/system/record-page/add-event/add-event.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Category} from '../../shared/models/category.model';
 import * as moment from 'moment';
 import {NgForm} from '@angular/forms';
@@ -8,13 +8,14 @@ import {BillService} from '../../shared/services/bill.service';
 import {Bill} from '../../shared/models/bill.model';
 import {mergeMap} from 'rxjs/operators';
 import {Message} from '../../../shared/models/message.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'rudkevycho-add-event',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.scss']
 })
-export class AddEventComponent implements OnInit {
+export class AddEventComponent implements OnInit, OnDestroy {
   @Input() categories: Category[] = [];
 
   types = [
@@ -23,6 +24,8 @@ export class AddEventComponent implements OnInit {
   ];
 
   message: Message;
+  sub1: Subscription;
+  sub2: Subscription;
 
   constructor(
     public eventsService: EventsService,
@@ -44,7 +47,7 @@ export class AddEventComponent implements OnInit {
     const newEvent = new RudkevychoEvent(type, Math.abs(amount), +category, moment()
       .format('DD.MM.YYYY HH:MM:SS'), description);
 
-    this.billService.getBill().subscribe((bill: Bill) => {
+    this.sub1 = this.billService.getBill().subscribe((bill: Bill) => {
       let value = 0;
       if (type === 'outcome') {
         if (amount > bill.value) {
@@ -56,7 +59,7 @@ export class AddEventComponent implements OnInit {
         value = bill.value + amount;
       }
 
-      this.billService.updateBill({value, currency: bill.currency})
+      this.sub2 = this.billService.updateBill({value, currency: bill.currency})
         .pipe(mergeMap(() => this.eventsService.addEvent(newEvent)))
         .subscribe(() => {
           form.setValue({
@@ -68,6 +71,16 @@ export class AddEventComponent implements OnInit {
         });
     });
 
+  }
+
+  ngOnDestroy() {
+    if (this.sub1) {
+      this.sub1.unsubscribe();
+    }
+
+    if (this.sub2) {
+      this.sub2.unsubscribe();
+    }
   }
 
 }
